857a8932eb7b52a8229988b793e3c5391ad46c97
Kevin Lin <stuy.kevinlin@gmail.com>
2020-03-02 10:57:07 -0500

    Help command and DB funcs

--- bot.py
+++ bot.py
@@ -2,6 +2,58 @@ import os
 import discord
 import database
 
+def helpFunc(message, splitcontent):
+    output = ""
+    if len(splitcontent) > 2:
+        cmd = splitcontent[2].lower()
+        if cmd in COMMAND_SET:
+            output = f"{COMMAND_SET[cmd]['helpmsg']}\n{COMMAND_SET[cmd]['usage']}"
+        else:
+            output = 'Invalid command, for a list of commands, use !market help'
+    else:
+        for i in sorted(COMMAND_SET.keys()):
+            output += f"{i} : {COMMAND_SET[i]['helpmsg']}\n"
+    return output
+
+def listFunc(message, splitcontent):
+    data = {
+        'name:' : [],
+        'notes:' : [],
+        'tags:' : []
+    }
+    curr = ""
+    for i in range(2, len(splitcontent)):
+        if splitcontent[i] in data:
+            curr = splitcontent[i]
+        elif curr != "":
+            data[curr].append(splitcontent[i])
+    if len(data['tags:']) > 10:
+        return "Too many tags"
+    for i in data['tags:']:
+        if len(i) > 32:
+            return "One or more tags are too long"
+    data['notes:'] = ' '.join(data['notes:'])
+    if len(data['notes:']) > 300:
+        return "Notes too long"
+    outputName = ' '.join(data['name:'])
+    if len(outputName) > 64:
+        return "Name too long"
+    return str(data)
+
+
+COMMAND_SET = {
+    'help' : {
+        'helpmsg' : 'Prints out the list of commands available, !market help <cmd> for command usage',
+        'usage' : '!market help <cmd>',
+        'function' : helpFunc
+    },
+    'list' : {
+        'helpmsg' : 'Lists a new item',
+        'usage' : '!market list name: <item name (max 64 characters)> notes: <notes (max 300 characters)> tags: <tag1 (max 32 characters)> <tag2> ... <tag10>',
+        'function' : listFunc
+    }
+}
+
 print("Starting Market Bot")
 
 client = discord.Client()
@@ -15,6 +67,14 @@ async def on_message(message):
     if message.author == client.user: #Ignore messages by self
         return
     
+    if message.content.startswith('!market'):
+        splitcontent = message.content.split()
+        if len(splitcontent) <= 1 or splitcontent[1].lower() not in COMMAND_SET:
+            await message.channel.send('Invalid command, for a list of commands, use !market help')
+            return
+        else:
+            await message.channel.send(COMMAND_SET[splitcontent[1].lower()]['function'](message, splitcontent))
+
     # await message.channel.send(output)
 
 
--- database.py
+++ database.py
@@ -6,62 +6,55 @@ DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cursor = conn.cursor()
 
-def setup():
-    cursor.execute(
-        '''
-        CREATE TABLE IF NOT EXISTS listings(
-            listingID SERIAL PRIMARY KEY,
-            server TEXT NOT NULL,
-            listerID TEXT NOT NULL,
-            itemName VARCHAR(64) NOT NULL,
-            price NUMERIC NOT NULL,
-            notes TEXT,
-            timeAdded TIMESTAMPTZ NOT NULL DEFAULT NOW()
-        )
-        '''
-    )
-    cursor.execute(
-        '''
-        CREATE TABLE IF NOT EXISTS tags(
-            server TEXT NOT NULL,
-            listingID SERIAL REFERENCES listings(listingID) ON DELETE CASCADE,
-            tag VARCHAR(32) NOT NULL
-        )
-        '''
-    )
-    cursor.execute(
-        '''
-        CREATE OR REPLACE FUNCTION expireRows() RETURNS void AS $$
-            BEGIN
-                DELETE FROM listings WHERE timeAdded < NOW() - INTERVAL '3 days';
-            END;
-        $$
-        LANGUAGE plpgsql;
-        '''
-    )
-
-setup()
-
-def addListing(server, listerID, itemName, price, notes, tags):
+def addListing(marketID, listerID, itemName, price, notes, tags):
     if len(tags) > 10: # Too many tags
         return False
-    cursor.execute("SELECT * FROM listings WHERE server = %s & listerID = %s LIMIT 10", (server, listerID,))
+    cursor.execute("SELECT * FROM listings WHERE marketID = %s & listerID = %s LIMIT 10", (marketID, listerID,))
     if len(cursor.fetchall() == 10): # Too many existing listings
         return False
     cursor.execute(
         '''
-        INSERT INTO listings (server, listerID, itemName, price, notes)
-        VALUES (%s, %s, %s, %s, %s)
+        INSERT INTO listings (marketID, listerID, itemName, price, notes, tags)
+        VALUES (%s, %s, %s, %s, %s, %s)
         RETURNING listingID
         ''',
-        (server, listerID, itemName, price, notes,)
+        (marketID, listerID, itemName, price, notes, tuple(tags),)
     )
-    listingID = cursor.fetchone()[0]
-    for i in tags:
-        cursor.execute("INSERT INTO tags (server, listingID, tag) VALUES (%s, %s, %s)", (server, listingID, i,))
+    return True
 
 def removeListing(listingID):
     cursor.execute("DELETE FROM listings WHERE listingID = %s", (listingID,))
+    return cursor.rowcount == 1
+
+def search(marketID, query):
+    cursor.execute(
+        '''
+        SELECT * FROM listings
+        WHERE
+            marketID = %s AND (
+                itemName ILIKE %s OR
+                notes ILIKE %s
+            )
+        LIMIT 10
+        ''',
+        (marketID, query, query,)
+    )
+    return cursor.fetchall()
+
+def tagsearch(marketID, tags):
+    if len(tags) > 10: # Too many tags
+        return False
+    cursor.execute(
+        '''
+        SELECT * FROM listings
+        WHERE
+            marketID = %s AND
+            tags @> %s
+        LIMIT 10
+        ''',
+        (marketID, tuple(tags),)
+    )
+    return cursor.fetchall()
 
 @atexit.register
 def saveChanges():
